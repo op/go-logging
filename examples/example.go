7ed74c1df690237d512c946c089375714dc2fa43
Steve Sanders <steve@keyba.se>
2015-12-04 17:44:49 -0800

    Bring back doFmtVerbLevelColor on Windows

--- examples/example.go
+++ examples/example.go
@@ -27,7 +27,8 @@ func main() {
 	// For demo purposes, create two backend for os.Stderr.
 	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
 	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
-
+	backend1.Color = true
+	backend2.Color = true
 	// For messages written to backend2 we want to add some additional
 	// information to the output, including the used log level and the name of
 	// the function.
