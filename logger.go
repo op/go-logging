02b8acafc77c8a38a9b75a6722e9362b2b18eb06
江林锦 <jianglj@quyun.com>
2015-12-08 00:56:38 +0800

    Fix concurrent data race problem

--- logger.go
+++ logger.go
@@ -159,7 +159,9 @@ func (l *Logger) log(lvl Level, format string, args ...interface{}) {
 		return
 	}
 
+	defaultBackendMutex.Lock()
 	defaultBackend.Log(lvl, 2+l.ExtraCalldepth, record)
+	defaultBackendMutex.Unlock()
 }
 
 // Fatal is equivalent to l.Critical(fmt.Sprint()) followed by a call to os.Exit(1).
