9f46785fd46a427f6ecfee2cb1969a6d1cffe88c
Elvis de Freitas <edigitalb@gmail.com>
2017-08-26 14:40:14 -0300

    formatting

--- trace.go
+++ trace.go
@@ -10,23 +10,23 @@ import (
 
 func GetCallerName(backLevel int) string {
 
-pc := make([]uintptr, 10)  // at least 1 entry needed
-runtime.Callers(backLevel + 2, pc)
-f := runtime.FuncForPC(pc[0])
-return f.Name();
+	pc := make([]uintptr, 10)  // at least 1 entry needed
+	runtime.Callers(backLevel + 2, pc)
+	f := runtime.FuncForPC(pc[0])
+	return f.Name();
 
 }
 
 // returns only function name
 func GetCallerFunctionName(backLevel int) string {
-caller := GetCallerName(backLevel + 1)
-sp := strings.Split(caller, ".")
+	caller := GetCallerName(backLevel + 1)
+	sp := strings.Split(caller, ".")
 
-if(len(sp) == 0){
-return ""
-}
+	if (len(sp) == 0) {
+		return ""
+	}
 
-return  sp[len(sp) - 1]
+	return sp[len(sp) - 1]
 }
 
 
@@ -34,14 +34,14 @@ return  sp[len(sp) - 1]
 // entao busca em um level acima
 func GetCallerFunctionNameSkippingAnnonymous(backlevel int) string {
 
-var name string = "";
-counter :=0
-for tryAgain := true; tryAgain ; counter++ {
-name = GetCallerFunctionName(backlevel + 1 + counter)
-rx, _ := regexp.Compile("^func\\d+")
-tryAgain = rx.MatchString(name)
-}
+	var name string = "";
+	counter := 0
+	for tryAgain := true; tryAgain; counter++ {
+		name = GetCallerFunctionName(backlevel + 1 + counter)
+		rx, _ := regexp.Compile("^func\\d+")
+		tryAgain = rx.MatchString(name)
+	}
 
-return name
+	return name
 
 }
