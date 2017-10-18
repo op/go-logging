876346c94a29f22a31066e253d7ee7470d8c0623
Elvis de Freitas Souza <edigitalb@gmail.com>
2017-10-18 12:11:29 -0200

    switching to new log structure

--- contextlogger.go
+++ contextlogger.go
@@ -2,48 +2,86 @@ package logging
 
 import (
 	"bytes"
+	"log"
+	"os"
 )
 
+type Printer interface {
+	Printf(format string, args ... interface{})
+}
+
+type Log interface {
+
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+
+	//Info(args ...interface{})
+	//Infof(format string, args ...interface{})
+	//
+	//Warning(args ...interface{})
+	//Warningf(format string, args ...interface{})
+	//
+	//Error(args ...interface{})
+	//Errorf(format string, args ...interface{})
+
+}
+
 type nativeLogger struct {
-	logger Log
+	writer Printer
+}
+
+type logPrinter struct {
+	log *log.Logger
+}
+
+func New() Log {
+	l := &nativeLogger{&logPrinter{log:log.New(os.Stderr, "", log.LstdFlags)}}
+	return l;
+}
+
+func(p *logPrinter) Printf(format string, args ... interface{}) {
+	p.log.Printf(format, args)
 }
 
 func (l *nativeLogger) Debug(args ...interface{}) {
-	l.logger.Debug(withMethod("")...)
+	l.writer.Printf(withCallerMethod(withLevel(new(bytes.Buffer), "DEBUG")).String(), args...)
 }
 
 func (l *nativeLogger) Debugf(format string, args ...interface{}) {
-	l.logger.Debugf(withMethod(format), args...)
+	l.writer.Printf(withCallerMethod(withLevel(new(bytes.Buffer), "DEBUG")).String(), args...)
 }
 
 func (l *nativeLogger) Info(args ...interface{}) {
-	l.logger.Info(withMethod("")...)
+	//l.writer.Printf(withCallerMethod(""), args...)
 }
 func (l *nativeLogger) Infof(format string, args ...interface{}) {
-	l.logger.Infof(withMethod(format), args...)
+	//l.writer.Printf(withCallerMethod(format), args...)
 }
 
 func (l *nativeLogger) Warning(args ...interface{}) {
-	l.logger.Warning(withMethod("")...)
+	//l.writer.Printf(withCallerMethod(""), args...)
 }
 func (l *nativeLogger) Warningf(format string, args ...interface{}) {
-	l.logger.Warningf(withMethod(format), args...)
+	//l.writer.Printf(withCallerMethod(format), args...)
 }
 
 func (l *nativeLogger) Error(args ...interface{}) {
-	l.logger.Error(withMethod("")...)
+	//l.writer.Printf(withCallerMethod(""), args...)
 }
 func (l *nativeLogger) Errorf(format string, args ...interface{}) {
-	l.logger.Errorf(withMethod(format), args...)
+	//l.writer.Printf(withCallerMethod(format), args...)
 }
 
-
 const level = 2
-func withMethod(append string) string {
-	var buffer bytes.Buffer
-	buffer.WriteString("m=")
-	buffer.WriteString(GetCallerFunctionNameSkippingAnnonymous(level))
-	buffer.WriteString(" ")
-	buffer.WriteString(append)
-	return buffer.String()
+func withCallerMethod(buff *bytes.Buffer) *bytes.Buffer {
+	buff.WriteString("m=")
+	buff.WriteString(GetCallerFunctionNameSkippingAnnonymous(level))
+	buff.WriteString(" ")
+	return buff;
+}
+
+func withLevel(buff *bytes.Buffer, lvl string) *bytes.Buffer {
+	buff.WriteString(lvl)
+	buff.WriteString(" ")
+	return buff;
 }
--- contextlogger_test.go
+++ contextlogger_test.go
@@ -1,51 +1,52 @@
 package logging
 
-import (
-	"testing"
-	"bytes"
-	"fmt"
-)
+import "testing"
 
-func TestNewLogWithLogger(t *testing.T) {
-
-	buff := bytes.Buffer{}
-
-	logger := MustGetLogger("main")
-	SetFormatter(MustStringFormatter(`%{level:.3s} %{message}`))
-	SetBackend(NewLogBackend(&buff, "", 0))
-
-	NewLogWithLogger(logger).Info("x1")
-	NewLogWithLogger(logger).Infof("x2")
-
-	expectedLog := "INF id=1, m=TestNewLogWithLogger x1\nINF id=2, m=TestNewLogWithLogger x2\n"
-	if buff.String() != expectedLog {
-		t.Fail()
-		t.Error("current: " + buff.String())
-		t.Error("expected: " + expectedLog)
-	}
-	fmt.Println(buff.String())
-
+func TestDebug(t *testing.T){
+	logger := New()
+	logger.Debugf("name=%s", "elvis");
 }
 
-func TestNewLog(t *testing.T) {
-
-	buff := bytes.Buffer{}
-
-	SetFormatter(MustStringFormatter(`%{level:.3s} %{message}`))
-	SetBackend(NewLogBackend(&buff, "", 0))
-
-	NewLog(NewContext()).Info("y1")
-	NewLog(NewContext()).Infof("y2")
-
-	expectedLog := "INF id=3, m=TestNewLog y1\nINF id=4, m=TestNewLog y2\n"
-
-
-	if buff.String() != expectedLog {
-		t.Fail()
-		t.Error("current: " + buff.String())
-		t.Error("expected: " + expectedLog)
-	}
-	fmt.Println(buff.String())
-}
-
-
+//func TestNewLogWithLogger(t *testing.T) {
+//
+//	buff := bytes.Buffer{}
+//
+//	logger := MustGetLogger("main")
+//	SetFormatter(MustStringFormatter(`%{level:.3s} %{message}`))
+//	SetBackend(NewLogBackend(&buff, "", 0))
+//
+//	NewLogWithLogger(logger).Info("x1")
+//	NewLogWithLogger(logger).Infof("x2")
+//
+//	expectedLog := "INF id=1, m=TestNewLogWithLogger x1\nINF id=2, m=TestNewLogWithLogger x2\n"
+//	if buff.String() != expectedLog {
+//		t.Fail()
+//		t.Error("current: " + buff.String())
+//		t.Error("expected: " + expectedLog)
+//	}
+//	fmt.Println(buff.String())
+//
+//}
+//
+//func TestNewLog(t *testing.T) {
+//
+//	buff := bytes.Buffer{}
+//
+//	SetFormatter(MustStringFormatter(`%{level:.3s} %{message}`))
+//	SetBackend(NewLogBackend(&buff, "", 0))
+//
+//	NewLog(NewContext()).Info("y1")
+//	NewLog(NewContext()).Infof("y2")
+//
+//	expectedLog := "INF id=3, m=TestNewLog y1\nINF id=4, m=TestNewLog y2\n"
+//
+//
+//	if buff.String() != expectedLog {
+//		t.Fail()
+//		t.Error("current: " + buff.String())
+//		t.Error("expected: " + expectedLog)
+//	}
+//	fmt.Println(buff.String())
+//}
+//
+//
