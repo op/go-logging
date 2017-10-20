2abd6978b72f8caf5cb090329b112bdbe3756092
Elvis de Freitas Souza <edigitalb@gmail.com>
2017-10-20 19:41:45 -0200

    increasing performance

--- pkg/trace/trace.go
+++ pkg/trace/trace.go
@@ -10,17 +10,15 @@ var rx, _ = regexp.Compile("\\.func\\d+$")
 func GetCallerFunctionName(backLevel int) string {
 
 	backLevel += 2
-	var pc = make([]uintptr, backLevel  + 3)
+	var pc, tryAgain = make([]uintptr, backLevel  + 3), true
 	runtime.Callers(backLevel, pc)
-	tryAgain := true
 	var fn *runtime.Func
 	for i:=0; i < len(pc) && tryAgain; i++ {
 		fn = runtime.FuncForPC(pc[i])
 		tryAgain = rx.MatchString(fn.Name())
-		//log.Printf("for, tryAgain=%v, name=%s", tryAgain, fn.Name())
 	}
-	if sp := strings.Split(fn.Name(), "."); len(sp) != 0 {
-		return sp[len(sp) - 1]
+	if index := strings.LastIndex(fn.Name(), "."); index != -1 {
+		return fn.Name()[index + 1:]
 	}
 	return ""
 }
