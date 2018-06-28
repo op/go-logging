7fe5d91f6cec396a45a96fb3ed225dafb5a9bbda
Kiel Friedt <friedt2@llnl.gov>
2018-06-28 09:40:31 -0700

    CMCOM-1559, syslog setup

--- syslog.go
+++ syslog.go
@@ -19,7 +19,9 @@ type SyslogBackend struct {
 // launched command.
 func NewSyslogBackend(prefix string) (b *SyslogBackend, err error) {
 	var w *syslog.Writer
-	w, err = syslog.New(syslog.LOG_CRIT, prefix)
+	if w, err = syslog.Dial("udp", "syslog-server:514", syslog.LOG_CRIT, prefix); err != nil {
+		panic(err)
+	}
 	return &SyslogBackend{w}, err
 }
 
@@ -27,7 +29,9 @@ func NewSyslogBackend(prefix string) (b *SyslogBackend, err error) {
 // syslog priority, like syslog.LOG_LOCAL3|syslog.LOG_DEBUG etc.
 func NewSyslogBackendPriority(prefix string, priority syslog.Priority) (b *SyslogBackend, err error) {
 	var w *syslog.Writer
-	w, err = syslog.New(priority, prefix)
+	if w, err = syslog.Dial("udp", "syslog-server:514", priority, prefix); err != nil {
+		panic(err)
+	}
 	return &SyslogBackend{w}, err
 }
 
